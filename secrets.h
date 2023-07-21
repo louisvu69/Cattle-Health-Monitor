5f1ef23c389941a69cf69ce4ecf6da26fdac5c90
Vu Tuan Linh <100025200+louisvu69@users.noreply.github.com>
2023-07-21 11:43:37 +0700

    update

--- secrets.h
+++ secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "P406"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "khongnoiday"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "123"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "123"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 2196082			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "TQYYF39XU1YINRKE"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 123123			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YOUR API"   // replace XYZ with your channel write API Key
